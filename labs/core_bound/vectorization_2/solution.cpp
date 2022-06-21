48932f6dc407edc289ddf41500c74b196115dc23
Deepanjan Biswas <deepanjan.biswas@formlabs.com>
2022-06-21 13:08:51 -0400

    Vectorize checksum summation. More than 5X speed gain

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,10 +1,21 @@
 #include "solution.hpp"
 
 uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
-  for (auto value : blob) {
-    acc += value;
-    acc += acc < value; // add carry
+  std::array<uint16_t, N / 16> sums;
+  sums.fill(0);
+  for (int ii = 0; ii < N / 16; ++ii) {
+    const auto offsetIdx = ii * 16;
+    for (int jj = 0; jj < 16; ++jj) {
+      sums[jj] += blob[offsetIdx + jj];
+      sums[jj] += sums[jj] < blob[offsetIdx + jj];  // add carry
+    }
   }
+
+  uint16_t acc = 0;
+  for (auto sum : sums) {
+    acc += sum;
+    acc += acc < sum;  // add carry
+  }
+
   return acc;
 }
